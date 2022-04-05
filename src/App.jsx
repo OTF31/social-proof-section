3f2cc63452f70f95fceb85ffdd3d81b11d04c1cd
denis <182906@unsaac.edu.pe>
2022-04-05 11:12:32 -0500

    Add mobile design
    Three bottom cards were added

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,21 @@ const App = () => {
       name: "Colton Smith",
       subtitle: "Verified Buyer",
       opinion:
-        "We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!",
+        " We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent! ",
     },
     {
       img: IreneProfile,
       name: "Irene Roberts",
       subtitle: "Verified Buyer",
       opinion:
-        "Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.",
+        " Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.",
     },
     {
       img: AnneProfile,
       name: "Anne Wallace",
       subtitle: "Verified Buyer",
       opinion:
-        "Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!",
+        " Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone! ",
     },
   ];
 
@@ -55,7 +55,7 @@ const App = () => {
           </div>
         </div>
 
-        <div>
+        <div className="mt-[50px] flex flex-col gap-[20px]">
           {buyers.map((buyer) => {
             return (
               <Card
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,22 @@
 const Card = (props) => {
   return (
     <>
-      <div className="bg-[#501F50]">
-        <img src={props.img} />
-        <p>{props.name}</p>
-        <p>{props.subtitle}</p>
-        <p>{props.opinion}</p>
+      <div className="bg-[#501F50] rounded-lg flex flex-col pl-[30px] pr-[33.5px] pt-[43px] pb-[33px]">
+        <div className="flex gap-[25px]">
+          <img
+            src={props.img}
+            alt="Profile"
+            className="block rounded-full w-[40px] h-[40px]"
+          />
+          <div>
+            <p className="text-white font-bold text-[0.85rem]">{props.name}</p>
+            <p className="text-[#EE67A4] text-[0.80rem]">{props.subtitle}</p>
+          </div>
+        </div>
+
+        <p className="text-[#F7F2F7] text-[0.85rem] font-[500] mt-[28px] tracking-tighter leading-[21.5px]">
+          &#8220;{props.opinion}&#8221;
+        </p>
       </div>
     </>
   );
